78d1aef546b65f2b97aef20de7b94fd1ad568128
Pete Jacobsson <pt.jacobsson@gmail.com>
2024-06-06 22:00:03 +0200

    Updated init

--- src/gdgtm/__init__.py
+++ src/gdgtm/__init__.py
@@ -1,5 +1,12 @@
-from .gdgtm_core import get_chelsa_daily, reproject_raster, change_raster_res, set_raster_boundbox, get_cognames_from_stac_coll_static, get_cogs_from_olm, align_raster, validate_raster_alignment, align_validate_raster, get_chelsa_bio_19812010_data, get_chelsa_clim_19812010_data
+from .gdgtm_chelsa_gets import get_chelsa_daily, get_chelsa_bio_19812010_data, get_chelsa_clim_19812010_data
+
+from .gdgtm_stac_gets import summarize_stac, get_cognames_from_stac_coll_static, get_cogs_from_olm 
+
+from .gdgtm_core import reproject_raster, change_raster_res, set_raster_boundbox, align_raster, validate_raster_alignment, align_validate_raster
 
 from .gdgtm_merge_mosaic import mosaic_rasters, merge_rasters
 
-from .gdgtm_stac_viewing import summarize_stac
+from .gdgtm_shapefiles import bound_shape, rasterize_shapefile_base, break_polys, add_polys_to_raster, rasterize_shapefile
+
+from .gdgtm_numpys import numpyify_raster, save_numpd_raster
+
