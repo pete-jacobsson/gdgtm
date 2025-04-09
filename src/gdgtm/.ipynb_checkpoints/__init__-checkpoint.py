36a7e8df17d5b020378b0879bfaef081f01d4656
Pete Jacobsson <pt.jacobsson@gmail.com>
2025-04-09 21:17:43 +0200

    Try push

--- src/gdgtm/.ipynb_checkpoints/__init__-checkpoint.py
+++ src/gdgtm/.ipynb_checkpoints/__init__-checkpoint.py
@@ -2,5 +2,5 @@
 
 from .transform import set_raster_boundbox, change_raster_res, apply_land_mask, rescale_raster_to_zero_one, replace_nodata_with_lowest, align_raster, validate_raster_alignment, reproject_raster, align_validate_raster, get_nearest_pixel, extract_window, check_window_values, random_square_from_raster, set_up_blank, get_proj_transform, check_if_geotif, align_rasters
 
-from .manage import convert_gtif_to_nparray, convert_gtif_to_jpg, process_dir_to_jpg, plot_raster_with_colorbar
+from .manage import convert_gtif_to_nparray, plot_raster_with_colorbar
 
